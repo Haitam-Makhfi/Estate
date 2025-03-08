96b563b024653711416717921538ffe7a1a4d9e4
Haitam Makhfi <haitammakhfi7@gmail.com>
2025-03-08 16:44:06 +0100

    finished feedback section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import logo from "./images/estate logo no-bg.png";
 import apartment from "./images/apartment.jpg";
 import { useEffect, useState } from "react";
@@ -9,6 +9,9 @@ import img3 from "./images/residence-2219972_1280.jpg";
 import img4 from "./images/villa-4555818_1280.jpg";
 import img5 from "./images/villa-6482427_1280.jpg";
 import img6 from "./images/villa-7303286_1280.jpg";
+import pfp1 from "./images/download.jfif";
+import pfp2 from "./images/download (2).jfif";
+import pfp3 from "./images/download (1).jfif";
 function App() {
   const projectImgs = [
     { img: img1, id: 1 },
@@ -18,6 +21,11 @@ function App() {
     { img: img5, id: 5 },
     { img: img6, id: 6 },
   ];
+  // const feedbackImages = [
+  //   { pfp: pfp1, id: 7 },
+  //   { pfp: pfp2, id: 8 },
+  //   { pfp: pfp3, id: 9 },
+  // ];
   const [counter, setCounter] = useState(0);
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -116,7 +124,7 @@ function App() {
             />
           </div>
         </section>
-        <section className="projects pop-left capitalize w-[70%] my-15 mx-auto translate-x-[20%]">
+        <section className="projects pop-left capitalize w-[70%] my-40 mx-auto translate-x-[20%]">
           <div className="title flex flex-col items-center mb-3">
             <h2>
               <b>projects</b>
@@ -159,7 +167,7 @@ function App() {
           </div>
         </section>
         <section className="feedback pop-left capitalize w-[70%] my-15 mx-auto translate-x-[20%] min-h-[100vh]">
-          <div className="title flex flex-col items-center mb-3">
+          <div className="title flex flex-col items-center mb-10">
             <h2>
               <b>customer</b>
               <span className="border-b-1 ml-3">Testimonials</span>
@@ -167,6 +175,76 @@ function App() {
             <p className="mt-3 w-70 text-center text-slate-500 text-sm">
               real stories from those who found home with us
             </p>
+          </div>
+          <div className="wraper flex gap-5">
+            <div className="card bg-white rounded-xs flex flex-col items-center justify-center px-5 py-13 shadow-xl">
+              <img
+                src={pfp1}
+                alt=""
+                className="pp w-17 h-17 rounded-[50%] object-cover mb-3"
+              />
+              <h3 className="text-lg font-bold">lora jackman</h3>
+              <span className="text-xs font-medium text-slate-600">
+                marketing manager
+              </span>
+              <div className="stars text-sky-600 text-sm my-3 flex gap-2">
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+              <p className=" text-[0.9rem] text-center font-medium text-slate-600">
+                from the very first meeting, they understood my vision and
+                helped me find the perfect property. their attention to detail
+                and commitlent to client satisfaction is unmatched
+              </p>
+            </div>
+            <div className="card bg-white rounded-xs flex flex-col items-center justify-center px-5 py-13 shadow-xl">
+              <img
+                src={pfp2}
+                alt=""
+                className="pp w-17 h-17 rounded-[50%] object-cover mb-3"
+              />
+              <h3 className="text-lg font-bold">richard nelson</h3>
+              <span className="text-xs font-medium text-slate-600">
+                marketing manager
+              </span>
+              <div className="stars text-sky-600 text-sm my-3 flex gap-2">
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+              <p className=" text-[0.9rem] text-center font-medium text-slate-600">
+                from the very first meeting, they understood my vision and
+                helped me find the perfect property. their attention to detail
+                and commitlent to client satisfaction is unmatched
+              </p>
+            </div>
+            <div className="card bg-white rounded-xs flex flex-col items-center justify-center px-5 py-13 shadow-xl">
+              <img
+                src={pfp3}
+                alt=""
+                className="pp w-17 h-17 rounded-[50%] object-cover mb-3"
+              />
+              <h3 className="text-lg font-bold">nicole washington</h3>
+              <span className="text-xs font-medium text-slate-600">
+                marketing manager
+              </span>
+              <div className="stars text-sky-600 text-sm my-3 flex gap-2">
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+              <p className=" text-[0.9rem] text-center font-medium text-slate-600">
+                from the very first meeting, they understood my vision and
+                helped me find the perfect property. their attention to detail
+                and commitlent to client satisfaction is unmatched
+              </p>
+            </div>
           </div>
         </section>
       </main>
